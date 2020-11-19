5a5e30393a94c5a8595c90b591ad5fc93589875f
PSH <slow_going77@naver.com>
2020-11-19 15:22:54 +0900

    profileupload

---
+++ _05_insta 이미지 업로드_피드_피드 무한 스크롤/instagram/src/main/java/com/cos/instagram/service/MyUserDetailService.java
@@ -0,0 +1,31 @@
+package com.cos.instagram.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.cos.instagram.model.User;
+import com.cos.instagram.repository.UserRepository;
+
+@Service
+public class MyUserDetailService implements UserDetailsService{
+
+	@Autowired
+	private UserRepository mUserRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = mUserRepository.findByUsername(username);
+		
+		MyUserDetail userDetails = null;
+		if(user != null) {
+			userDetails = new MyUserDetail();
+			userDetails.setUser(user);
+		} else {
+			throw new UsernameNotFoundException("Not Found'username'");	
+		} 
+		return userDetails;
+	}
+}
